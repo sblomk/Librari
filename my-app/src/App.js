0ab93cb9533265212f7d42e9462bcb64a3333922
davidtranaeus <davidtra@kth.se>
2018-04-12 11:58:12 +0200

    Importerat firebase

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,8 @@ import { modelInstance } from './data/BookModel'
 import Search from './Search/Search';
 import Book from './Book/Book';
 
+import firebase from './firebase.js';
+
 class App extends Component {
   constructor(props){
     super(props)
@@ -16,6 +18,15 @@ class App extends Component {
   }
 
   render() {
+
+    // https://console.firebase.google.com/project/librari-41dab/database/librari-41dab/data
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      name: "David",
+      age: 25
+    }
+    itemsRef.push(item);    
+
     return (
       <div className='App'>
         <Route exact path="/" component={Header}/>
