a53e450aa68f50c4f56abfc069edf471c68abd62
Camilla Ahlenius <camahl@kth.se>
2018-04-19 10:39:43 +0200

    kommentarer

--- my-app/src/Search/Search.js
+++ my-app/src/Search/Search.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import './Search.css';
 
+// This view contains the search bar
 
 var filter = '';
 class Search extends Component {
     constructor(props){
         super(props);
         
-    
         this.state = {
             status: 'INITIAL'
         }
         filter = '';
     }
 
+    // on change, this handler will update the search string set by the user
     handleChange = (event) => {
         console.log(event.target.value);
         filter = event.target.value;
         this.props.model.setFilter(filter);
-
     }
 
     render(){
