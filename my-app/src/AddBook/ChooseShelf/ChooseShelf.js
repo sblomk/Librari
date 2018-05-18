d18bdd18a0d98749f7a815949f85bb280ed6195b
swedhag <37803962+swedhag@users.noreply.github.com>
2018-05-18 16:32:41 +0200

    Comments in ChooseShelf.js

--- my-app/src/AddBook/ChooseShelf/ChooseShelf.js
+++ my-app/src/AddBook/ChooseShelf/ChooseShelf.js
@@ -7,20 +7,24 @@ class ChooseShelf extends Component {
     render(){
         let shelfList;
         let btn;
-
+           
+        //If feature is set to ChooseSelf in parent component AddBook.js and the
+        //user has previously saved shelves
         if(this.props.shelves != null){
             shelfList = this.props.shelves.map((shelf) => 
                 <option value={shelf.id} key={shelf.id}>
                 {shelf.name}
                 </option>
         )}
+        //Else if the user has no previously saved shelves
         else{
-            shelfList = <option value="">You don't have any shelves</option>
+            shelfList = <option value="">You don\'t have any shelves</option>
         }
-
+        //If the user has selected a shelf in the dropdown menu of AddBook.js
         if (this.props.activeShelf){
             btn = <button id="addbtn" className="addbtn" title="Add to shelf" onClick={this.props.submit}>Add</button>
         }
+        //Else do not enable the button
         else{
             btn = <button id="addbtn" className="addbtn" disabled title="You need to select a shelf!" onClick={this.props.submit}>Add</button>
         }
